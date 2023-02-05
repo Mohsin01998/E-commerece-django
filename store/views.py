928494b7091173f9400f4faa0463cb1581fcd7f5
mohsin01998 <mohsin24898@gmail.com>
2023-02-05 05:45:40 +0500

    changes

--- store/views.py
+++ store/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+import datetime
+# from .models import *
+# from .utils import cookieCart, cartData, guestOrder
 
 # Create your views here.
+def shope(request):
+    context={}
+    return render(request,'shope.html',context=context)
+
+def home(request):
+    return render(request,'index.html')
+def cart(request):
+    context={}
+    return render(request,'cart.html',context=context)
+
+def checkout(request):
+    context={}
+    return render(request,'checkout.html',context=context)
